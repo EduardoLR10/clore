e17a08d5b2a24d8f2a86a6181f27ce39c750d948
Marinho <marinho.pinotti@gmail.com>
2024-11-16 19:41:51 -0300

    Fix init name collision

--- src/clojure_demo/main.clj
+++ src/clojure_demo/main.clj
@@ -1,19 +1,21 @@
 (ns clojure-demo.main
   (:gen-class)
   (:require
+    [clojure-demo.core]
     [init.core :as init]
-    [init.discovery :as discovery]
-    [clojure-demo.core]))
+    [init.discovery :as discovery]))
 
-(def ^{:init/tags #{:datomic/uri}} bogus-uri
+(def bogus-uri
   "URI used by Datomic. In a proper project, you'd set this up on a separate
   process and using durable storage."
   "datomic:mem://clojure-demo")
 
-(def ^{:init/tags #{:datomic/fixtures}} no-fixtures [])
+(def no-fixtures [])
 
 (defn -main
   [& _]
   (-> (discovery/static-scan '[clojure-demo])
+    (discovery/bind {:datomic/uri #'bogus-uri
+                     :datomic/fixtures #'no-fixtures})
     init/start
     init/stop-on-shutdown))
